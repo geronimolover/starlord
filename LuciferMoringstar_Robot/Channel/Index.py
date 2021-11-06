c4eb5523c2c35bc2fa2af94e868b5b5fbeac4d71
vellithiragroup123 <88137446+vellithiragroup123@users.noreply.github.com>
2021-11-06 10:18:26 +0530

    Update Index.py

--- LuciferMoringstar_Robot/Channel/Index.py
+++ LuciferMoringstar_Robot/Channel/Index.py
@@ -17,7 +17,7 @@ async def index_files(bot, message):
         await message.reply('Wait until previous process complete.')
     else:
         while True:
-            last_msg = await bot.ask(text = "Forward me last message of a channel which I should save to my database.\n\nYou can forward posts from any public channel, but for private channels bot should be an admin in the channel.\n\nMake sure to forward with quotes (Not as a copy)", chat_id = message.from_user.id)
+            last_msg = await bot.ask(text = "Forward me last message of a channel which I should save to my database.\n\nYou can forward posts from any public channel, but for private channels bot should be an admin in the channel.\n\nMake sure to forward with quotes (Not as a copy)\n\nMake sure your channel has only movies and webseries. If your channel has any restricted contents like porn you and your channel will be reported and banned", chat_id = message.from_user.id)
             try:
                 last_msg_id = last_msg.forward_from_message_id
                 if last_msg.forward_from_chat.username:
